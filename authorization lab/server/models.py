273dfe4c6f28e0ce055ee82843235f0346d3ec7e
LisonOCipher <lisoncipher5@gmail.com>
2024-04-17 20:12:11 +0300

    Add files via upload

---
+++ authorization lab/server/models.py
@@ -0,0 +1,42 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy_serializer import SerializerMixin
+
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+
+db = SQLAlchemy(metadata=metadata)
+
+class Article(db.Model, SerializerMixin):
+    __tablename__ = 'articles'
+
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String)
+    title = db.Column(db.String)
+    content = db.Column(db.String)
+    preview = db.Column(db.String)
+    minutes_to_read = db.Column(db.Integer)
+    is_member_only = db.Column(db.Boolean, default=False)
+    date = db.Column(db.DateTime, server_default=db.func.now())
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    def __repr__(self):
+        return f'Article {self.id} by {self.author}'
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True)
+
+    articles = db.relationship('Article', backref='user')
+
+<<<<<<<<<<<<<<  ✨ Codeium Command 🌟 >>>>>>>>>>>>>>>>
+def __repr__(self):
+    if self.username is None or self.id is None:
+     raise ValueError("Username or ID is None")
+def __repr__(self):
+        return f'User {self.username}, ID {self.id}'
+<<<<<<<  bb912cc0-b09a-440b-ab5b-eb4285b55ff5  >>>>>>>
